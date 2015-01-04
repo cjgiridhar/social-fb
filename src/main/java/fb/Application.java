42266968c5feead52c73d1f3422bb77ca780d86a
Chetan <cjgiridhar@example.com>
2015-01-04 16:30:31 +0530

    Whole project

---
+++ src/main/java/fb/Application.java
@@ -0,0 +1,20 @@
+package fb;
+
+/**
+ * Created by cgiridhar on 04/01/15.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/fb/FBController.java
@@ -0,0 +1,40 @@
+package fb;
+/**
+ * Created by cgiridhar on 04/01/15.
+ */
+
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.inject.Inject;
+
+@Controller
+@RequestMapping("/")
+public class FBController {
+
+    private Facebook facebook;
+
+    @Inject
+    public FBController(Facebook facebook) {
+        this.facebook = facebook;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String helloFacebook(Model model) {
+        if (!facebook.isAuthorized()) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute(facebook.userOperations().getUserProfile());
+        PagedList<Post> homeFeed = facebook.feedOperations().getHomeFeed();
+        model.addAttribute("feed", homeFeed);
+
+        return "fb";
+    }
+
+}
